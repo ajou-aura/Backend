9e925854683a08e1371c08fab08bca10c08ea01b
origami0352 <kyn008116@gamil.com>
2025-07-23 15:52:16 +0900

    fix: CORS 에러 수정

--- src/main/java/sulhoe/ajouhub/config/CorsFilterConfig.java
+++ src/main/java/sulhoe/ajouhub/config/CorsFilterConfig.java
@@ -2,6 +2,8 @@ package sulhoe.ajouhub.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.*;
 import org.springframework.web.filter.CorsFilter;
 
@@ -14,6 +16,7 @@ public class CorsFilterConfig {
     private String[] allowedOrigins;
 
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public CorsFilter corsFilter() {
         CorsConfiguration cfg = new CorsConfiguration();
         cfg.setAllowedOrigins(List.of(allowedOrigins));

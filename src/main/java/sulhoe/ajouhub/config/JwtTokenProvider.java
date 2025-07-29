38610c14d64f28c9b6b3c5a08aab4fdf21a22c53
zerogod <code0.sin@gmail.com>
2025-07-29 21:05:56 +0900

    feat: token 만료 시간 1분으로

--- src/main/java/sulhoe/ajouhub/config/JwtTokenProvider.java
+++ src/main/java/sulhoe/ajouhub/config/JwtTokenProvider.java
@@ -19,7 +19,7 @@ public class JwtTokenProvider {
     private String secretKey;              // application.properties 에서 Base64로 인코딩된 키
 
     private SecretKey signingKey;
-    private static final long ACCESS_EXP  = 1000L * 60 * 60;        // 1시간
+    private static final long ACCESS_EXP  = 1000L;        // 1분
     private static final long REFRESH_EXP = 1000L * 60 * 60 * 24;   // 1일
     public static final long REFRESH_EXPIRY_SECONDS = REFRESH_EXP / 1000;  // 쿠키 maxAge용
 
